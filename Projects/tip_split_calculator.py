fad4eed722cfb35f95801591885ecc8e1e1792a9
Tim Oglesby <timj.oglesby@gmail.com>
2021-11-24 11:43:33 -0700

    initial commit
    Initial commit adding a few projects and readme file.

---
+++ Projects/tip_split_calculator.py
@@ -0,0 +1,12 @@
+
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("what percentage tip would you like to give? 15, 18, or 20? "))
+split = int(input("How many people to split the bill? "))
+
+bill_with_tip = tip / 100 * bill + bill
+
+per_person = bill_with_tip / split
+final_amount = "{:.2f}".format(per_person)
+
+print(f"Each person should pay: ${final_amount}")
---
+++ README.md
@@ -0,0 +1,1 @@
+This reposity contains my Python Projects starting with various projects from my 100 Days of Python coding challenge.
